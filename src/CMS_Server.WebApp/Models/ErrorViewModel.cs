d1adef89f00057bb3453289e2400abf94584cba2
NguyenNgocPhuong <phuong260722@gmail.com>
2025-06-01 17:08:07 +0700

    first commit

---
+++ src/CMS_Server.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMS_Server.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
